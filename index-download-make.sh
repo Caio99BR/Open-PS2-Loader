94843882d01827b8a0f494591cc10bf5fe061b40
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-23 01:14:14 -0200

    gh-pages: Fix a simple problem
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- index-download-make.sh
+++ index-download-make.sh
@@ -22,8 +22,8 @@ sed -i "s|\
 \t\t\t\t\t\t<span class='image left'><img src='https://github.com/${NEW_REPO_SLUG}.png' alt='' /></span>\n\
 \t\t\t\t\t\t<p>Release: ${OPL_DATE}</br>From: ${NEW_REPO_SLUG}</br>Build: #${TRAVIS_BUILD_NUMBER}</p>\n\
 \t\t\t\t\t\t<ul class='actions'>\n\
-\t\t\t\t\t\t\t<li><a href='${OPL_UP_LAST_BUILD}' class='button'>OPL Package<\/a><\/li>\n\
-\t\t\t\t\t\t\t<li><a href='${OPL_LANG_UP_LAST_BUILD}' class='button'>Lang Package<\/a><\/li>\n\
+\t\t\t\t\t\t\t<li><a href='${OPL_UP_LAST_BUILD}' class='button'>OpenPS2Loader Package<\/a><\/li>\n\
+\t\t\t\t\t\t\t<li><a href='${OPL_LANG_UP_LAST_BUILD}' class='button'>Language Package<\/a><\/li>\n\
 \t\t\t\t\t\t<\/ul>\n\
 \t\t\t\t\t</section>\
 |" index.html
