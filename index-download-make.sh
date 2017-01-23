d3fb78fedcf2934f630ee78ba5718563859c6642
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-23 00:23:16 -0200

    index-download-make.sh: Give access to people that current make changes
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- index-download-make.sh
+++ index-download-make.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # Auto make Download Section by Caio99BR <caiooliveirafarias0@gmail.com>
 
+if [[ ${NEW_REPO_SLUG} == "Caio99BR" || ${NEW_REPO_SLUG} == "ifcaro" ||
+      ${NEW_REPO_SLUG} == "sp193" || ${NEW_REPO_SLUG} == "ShaolinAssassin" ||
+      ${NEW_REPO_SLUG} == "uyjulian" || ${NEW_REPO_SLUG} == "Jay-Jay-OPL" ||
+      ${NEW_REPO_SLUG} == "BatRastard" || ${NEW_REPO_SLUG} == "AKuHAK" ||
+      ${NEW_REPO_SLUG} == "ElPatas1" || ${NEW_REPO_SLUG} == "rickgaiser" ||
+      ${NEW_REPO_SLUG} == "Dr-Q" || ${NEW_REPO_SLUG} == "ps2homebrew" ]]
+then
+	echo "You have access, continue :)"
+else
+	echo "Sorry, you clone not have access to upload here :("
+	exit 1
+fi
+
 sed -i "s\
 |\
 \t\t\t\t\t<\!-- Automatic Download Section -->\n\
@@ -15,3 +28,7 @@ sed -i "s\
 \t\t\t\t\t\t<\/ul>\n\
 \t\t\t\t\t</section>\n\
 |" index.html
+
+git add index.html
+git commit -m "[AUTOMATIC TRAVISCI COMMIT] BUILD: ${TRAVIS_BUILD_NUMBER} | FROM: ${NEW_REPO_SLUG}"
+git push -fq origin gh-pages > /dev/null
