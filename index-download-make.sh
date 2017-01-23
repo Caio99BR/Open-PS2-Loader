fecc3aefd444d214d7d0cde470cf9551df562668
Caio Oliveira <caiooliveirafarias0@gmail.com>
2017-01-23 00:02:28 -0200

    gh-pages: Update it :)
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- index-download-make.sh
+++ index-download-make.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Auto make Download Section by Caio99BR <caiooliveirafarias0@gmail.com>
 
-sed -i "s|\t\t\t\t\t\t<\!-- Download Section -->|\
-\t\t\t\t\t\t<\!-- Download Section -->\n\
-\t\t\t\t\t\t<p>Release: ${OPL_DATE} \| From: ${NEW_REPO_SLUG} \| Nightly \#${TRAVIS_BUILD_NUMBER}<\/p>\n\
+sed -i "s\
+|\
+\t\t\t\t\t<\!-- Automatic Download Section -->\n\
+|\
+\t\t\t\t\t<\!-- Automatic Download Section -->\n\
+\t\t\t\t\t<section>\n\
+\t\t\t\t\t\t<span class='image left'><img src='https://github.com/${NEW_REPO_SLUG}.png' alt='' /></span>\n\
+\t\t\t\t\t\t<p>Release: ${OPL_DATE}</br>From: ${NEW_REPO_SLUG}</br>Build: #${TRAVIS_BUILD_NUMBER}</p>\n\
 \t\t\t\t\t\t<ul class='actions'>\n\
 \t\t\t\t\t\t\t<li><a href='${OPL_UP_LAST_BUILD}' class='button'>OPL Package<\/a><\/li>\n\
 \t\t\t\t\t\t\t<li><a href='${OPL_LANG_UP_LAST_BUILD}' class='button'>Lang Package<\/a><\/li>\n\
-\t\t\t\t\t\t<\/ul>|" index.html
+\t\t\t\t\t\t<\/ul>\n\
+\t\t\t\t\t</section>\n\
+|" index.html
